20aef23f2638143ef6636441b1f8597b5da0976b
User <User@LAPTOP-TF5GU2MT>
2023-10-25 09:58:05 +0300

    Добавьте файлы проекта.

---
+++ WpfApp5/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Ribbon;
+
+namespace WpfApp5
+{
+	internal class Pair
+	{
+		private int _one;
+		private int _second;
+
+		public int One { 
+			get 
+			{ 
+				return _one;
+			}
+			set { 
+				_one = value;
+			} 
+		} // свойства
+		public int Second
+		{
+			get
+			{
+				return _second;
+			}
+			set
+			{
+				_second = value;
+			}
+		} // свойства
+
+	
+		public Pair(int one, int second)
+		{
+			_one = one;
+		   _second = second;
+		} // конструктор 
+
+		/// <summary>
+		/// Сложение полей пары чисел (первого числа пары с вторым)
+		/// </summary>
+		/// <returns>Результат суммы полей пары чисел</returns>
+		public int Sum ()
+		{
+			return _one + _second;
+		}
+		/// <summary>
+		/// Сложение этой пары чисел с другой парой чисел
+		/// </summary>
+		/// <param name="second">Вторая пара чисел</param>
+		/// <returns>Третья пара чисел (результаты суммы первой с второй)</returns>
+		public Pair Sum(Pair second)
+		{		 
+			return new Pair(One + second.One, Second + second.Second);
+        }
+	}
+}
---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+
+		private void Button_Сложить_1_Пару_Чисел(object sender, RoutedEventArgs e)
+		{
+			if (!int.TryParse(пара1_1.Text, out int a) || !int.TryParse(пара1_2.Text, out int b)) 
+			{
+                MessageBox.Show("НЕ НАДО ДЯДЯ.....ААААА!!!!");
+                return;
+					}
+
+			Pair pair = new Pair(a, b);
+			MessageBox.Show(pair.Sum().ToString());
+
+
+		}
+
+		private void Button_Сложить_2_Пару_Чисел_Click(object sender, RoutedEventArgs e)
+		{
+			if (!int.TryParse(пара2_1.Text, out int a) || !int.TryParse(пара2_2.Text, out int b))
+			{
+                MessageBox.Show("НЕ НАДО ДЯДЯ.......ААААА!!!!");
+                return;
+			}
+
+			Pair pair = new Pair(a, b);
+			MessageBox.Show(pair.Sum().ToString());
+
+		}
+
+		private void Button_Сложить_Пары_Чисел(object sender, RoutedEventArgs e)
+		{
+			if (!int.TryParse(пара1_1.Text, out int a)
+			|| !int.TryParse(пара1_2.Text, out int b)
+			|| !int.TryParse(пара2_1.Text, out int c)
+			|| !int.TryParse(пара2_2.Text, out int d)) {
+				MessageBox.Show("НЕ НАДО ДЯДЯ.....ААААА!!!!");
+			return;
+			}
+
+			Pair pair1 = new Pair(a, b);
+			Pair pair2 = new Pair(a, c);
+			Pair pair3 = pair1.Sum(pair2);
+			пара3_1.Text = pair3.One.ToString();
+			пара3_2.Text = pair3.Second.ToString();
+
+			
+		}
+
+		private void MenuItem_Click_О_программе(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Автор: Соколов Даниил \n Практическая работа №5 \n Задание:Создать класс Pair (пара чисел). Создать необходимые методы и свойства. \r\nОпределить методы метод сложения полей и операцию сложения пар (а, b) + (с, d) \r\n= (а + c, b + d). Создать перегруженные методы для увеличения полей на 1, сложения трех пар чисел.\r\n");
+
+        }
+
+		private void MenuItem_Click_Выход(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void MenuItem_Click_Анекдот(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Почему застрелился Гитлер - ему пришёл счет за газ.");
+		}
+	}
+}
+// Создать класс Pair (пара чисел). Создать необходимые методы и свойства. 
+// Определить методы метод сложения полей и операцию сложения пар (а, b) +(с, d)
+// = (а + c, b + d).Создать перегруженные методы для увеличения полей на 1,
+// сложения трех пар чисел.
