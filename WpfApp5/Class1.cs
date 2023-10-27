035e32f83ac099f004164c4a1d0d58ffd3276c35
User <User@LAPTOP-TF5GU2MT>
2023-10-27 15:02:04 +0300

    Пр_р_№6

--- WpfApp5/Class1.cs
+++ WpfApp5/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,14 +14,16 @@ namespace WpfApp5
 		private int _one;
 		private int _second;
 
-		public int One { 
-			get 
-			{ 
+		public int One
+		{
+			get
+			{
 				return _one;
 			}
-			set { 
+			set
+			{
 				_one = value;
-			} 
+			}
 		} // свойства
 		public int Second
 		{
@@ -34,18 +37,18 @@ namespace WpfApp5
 			}
 		} // свойства
 
-	
+
 		public Pair(int one, int second)
 		{
 			_one = one;
-		   _second = second;
+			_second = second;
 		} // конструктор 
 
 		/// <summary>
 		/// Сложение полей пары чисел (первого числа пары с вторым)
 		/// </summary>
 		/// <returns>Результат суммы полей пары чисел</returns>
-		public int Sum ()
+		public int Sum()
 		{
 			return _one + _second;
 		}
@@ -55,8 +58,32 @@ namespace WpfApp5
 		/// <param name="second">Вторая пара чисел</param>
 		/// <returns>Третья пара чисел (результаты суммы первой с второй)</returns>
 		public Pair Sum(Pair second)
-		{		 
+		{
 			return new Pair(One + second.One, Second + second.Second);
-        }
+		}
+
+		/// <summary>
+		/// Сложение пар чисел
+		/// </summary>
+		/// <param name="pair1">Первая пара чисел</param>
+		/// <param name="pair2">Вторая пара чисел</param>
+		/// <returns>Третья пара чисел (результаты суммы первой с второй)</returns>
+		public static Pair operator +(Pair pair1, Pair pair2)
+		{
+			return new Pair(pair1.One + pair2.One, pair1.Second + pair2.Second);
+		}
+
+		/// <summary>
+		/// Уменьшение пары чисел на 1
+		/// </summary>
+		/// <param name="pair"></param>
+		/// <returns>Пара чисел с полями на единицу меньше</returns>
+		public static Pair operator --(Pair pair)
+		{
+			pair.One--;
+			pair.Second--;
+
+			return pair;
+		}
 	}
 }
--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -25,32 +25,40 @@ namespace WpfApp5
 			InitializeComponent();
 		}
 
+		const string IncorrectInputError = "Не надо дядя.....Няа!!! OwO ";
 
 		private void Button_Сложить_1_Пару_Чисел(object sender, RoutedEventArgs e)
 		{
-			if (!int.TryParse(пара1_1.Text, out int a) || !int.TryParse(пара1_2.Text, out int b)) 
+			if (!int.TryParse(пара1_1.Text, out int a) || !int.TryParse(пара1_2.Text, out int b))
 			{
-                MessageBox.Show("НЕ НАДО ДЯДЯ.....ААААА!!!!");
-                return;
-					}
+				MessageBox.Show(IncorrectInputError);
+				return;
+			}
 
-			Pair pair = new Pair(a, b); 
-			MessageBox.Show("Сумма 1-ой пары чисел:" + pair.Sum().ToString());
+			Pair pair = new Pair(a, b);
+			pair--;
+			пара1_1.Text = pair.One.ToString();
+			пара1_2.Text = pair.Second.ToString();
 
-
+			пара3_1.Clear();
+			пара3_2.Clear();
 		}
 
 		private void Button_Сложить_2_Пару_Чисел_Click(object sender, RoutedEventArgs e)
 		{
 			if (!int.TryParse(пара2_1.Text, out int a) || !int.TryParse(пара2_2.Text, out int b))
 			{
-                MessageBox.Show("НЕ НАДО ДЯДЯ.......ААААА!!!!");
-                return;
+				MessageBox.Show(IncorrectInputError);
+				return;
 			}
 
 			Pair pair = new Pair(a, b);
-			MessageBox.Show("Сумма 2-ой пары чисел:" + pair.Sum().ToString());
+			pair--;
+			пара2_1.Text = pair.One.ToString();
+			пара2_2.Text = pair.Second.ToString();
 
+			пара3_1.Clear();
+			пара3_2.Clear();
 		}
 
 		private void Button_Сложить_Пары_Чисел(object sender, RoutedEventArgs e)
@@ -58,25 +66,29 @@ namespace WpfApp5
 			if (!int.TryParse(пара1_1.Text, out int a)
 			|| !int.TryParse(пара1_2.Text, out int b)
 			|| !int.TryParse(пара2_1.Text, out int c)
-			|| !int.TryParse(пара2_2.Text, out int d)) {
-				MessageBox.Show("НЕ НАДО ДЯДЯ.....ААААА!!!!");
-			return;
+			|| !int.TryParse(пара2_2.Text, out int d))
+			{
+				MessageBox.Show(IncorrectInputError);
+				return;
 			}
 
 			Pair pair1 = new Pair(a, b);
 			Pair pair2 = new Pair(c, d);
-			Pair pair3 = pair1.Sum(pair2);
+			Pair pair3 = pair1 + pair2;
 			пара3_1.Text = pair3.One.ToString();
 			пара3_2.Text = pair3.Second.ToString();
 
-			
+
 		}
 
 		private void MenuItem_Click_О_программе(object sender, RoutedEventArgs e)
 		{
-			MessageBox.Show("Автор: Соколов Даниил \n Практическая работа №5 \n Задание:Создать класс Pair (пара чисел). Создать необходимые методы и свойства. \r\nОпределить методы метод сложения полей и операцию сложения пар (а, b) + (с, d) \r\n= (а + c, b + d). Создать перегруженные методы для увеличения полей на 1, сложения трех пар чисел.\r\n");
+			MessageBox.Show("Автор: Соколов Даниил \n" +
+			"Практическая работа №6 \nЗадание:Использовать класс Pair (пара чисел).\n" +
+			"Разработать операцию сложения пар (а, b) + (с, d) = (а + c, b + d).\n" +
+			"Разработать операцию для уменьшения полей на 1.");
 
-        }
+		}
 
 		private void MenuItem_Click_Выход(object sender, RoutedEventArgs e)
 		{
@@ -85,11 +97,7 @@ namespace WpfApp5
 
 		private void MenuItem_Click_Анекдот(object sender, RoutedEventArgs e)
 		{
-			MessageBox.Show("Почему застрелился Гитлер - ему пришёл счет за газ.");
+			MessageBox.Show("Если вам уже за тридцать и вы абсолютно ничего не добились в этой жизни — значит, \n вы честный и порядочный человек.");
 		}
 	}
 }
-// Создать класс Pair (пара чисел). Создать необходимые методы и свойства. 
-// Определить методы метод сложения полей и операцию сложения пар (а, b) +(с, d)
-// = (а + c, b + d).Создать перегруженные методы для увеличения полей на 1,
-// сложения трех пар чисел.
